a50bd1e4df11baa8e266f0bc2ad0c3952d3b231f
Julian Irwin <julian.irwin@gmail.com>
2015-02-19 14:21:27 -0600

    added some color sets

---
+++ colorcycles.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# blue to red diverging set
+blue_to_red_diverging = ['#d7191c', '#fdae61', '#ffffbf', '#abd9e9', '#2c7bb6']
+
+# red oragne sequential single hue:
+orange_sequential = ['#fff5eb', '#fee6ce', '#fdd0a2', '#fdae6b', '#fd8d3c',
+                     '#f16913', '#d94801', '#a63603', '#7f2704']
+
+# blue sequential single hue:
+blue_sequential = ['#f7fbff', '#deebf7', '#c6dbef', '#9ecae1', '#6baed6',
+                   '#4292c6', '#2171b5', '#08519c', '#08306b']
+
+mpl_default = ['b', 'r', 'g', 'c', 'm', 'y', 'k']
+
+# Cynthia Brewer Set 1
+categorical_1 = [ '#e41a1c', '#377eb8', '#4daf4a', '#ff7f00', '#a65628',
+                  '#f781bf', '#999999', '#984ea3', '#ffff33'] 
+
+# Cynthia Brewer Set 2
+categorical_2 = ['#66c2a5', '#fc8d62', '#8da0cb', '#e78ac3', '#a6d854',
+                 '#ffd92f', '#e5c494', '#b3b3b3' ]
+
+# Dark Brewer Set 2
+categorical_3 = ['#1b9e77', '#d95f02', '#7570b3', '#e7298a', '#66a61e',
+                 '#e6ab02', '#a6761d', '#666666']
