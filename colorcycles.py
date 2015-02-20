a1a0c43d94a1e0a57fc9fb86d6661f0f87a4858f
Julian Irwin <julian.irwin@gmail.com>
2015-02-20 15:26:39 -0600

    added categorical 3

--- colorcycles.py
+++ colorcycles.py
@@ -24,3 +24,4 @@ categorical_2 = ['#66c2a5', '#fc8d62', '#8da0cb', '#e78ac3', '#a6d854',
 # Dark Brewer Set 2
 categorical_3 = ['#1b9e77', '#d95f02', '#7570b3', '#e7298a', '#66a61e',
                  '#e6ab02', '#a6761d', '#666666']
+
